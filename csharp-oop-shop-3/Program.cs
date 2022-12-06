d17ed919405d56138ed97d46b111767e3f13a7c0
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-12-06 16:14:35 +0100

    testato TRY CATCH di prodotto1 sul Main

--- csharp-oop-shop-3/Program.cs
+++ csharp-oop-shop-3/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using csharp_oop_shop_3;
+using csharp_oop_shop_3.Exceptions;
+
+try {
+    Prodotto prodotto1 = new Prodotto("Prodotto 1", "Prodotto 1", 1.50, 22);
+    prodotto1.StampaProdotto();
+
+    prodotto1.SetNome("");
+    prodotto1.StampaProdotto();
+
+    Console.WriteLine("Cambiamo il prezzo del prodotto 1");
+    prodotto1.SetPrezzo(-1.50);
+    prodotto1.StampaProdotto();
+
+} catch (NomeVuotoException e) {
+    Console.WriteLine(e.Message);
+} catch (PrezzoNegativoException e) {
+    Console.WriteLine(e.Message);
+}
+
+Console.WriteLine(Prodotto.numeroProdotti);
